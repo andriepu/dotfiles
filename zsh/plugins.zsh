4b21992db4097fe6cf172e7faa9f3cf5c09b6fe6
andriepu <andriepu@gmail.com>
2018-04-04 11:05:50 +0700

    remove unused

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -8,7 +8,6 @@ zplug "djui/alias-tips"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "Tarrasch/zsh-bd"
 zplug "ael-code/zsh-colored-man-pages"
-zplug "johnhamelink/rvm-zsh"
 zplug "lib/clipboard", from:oh-my-zsh
 zplug "plugins/git", from:oh-my-zsh
 zplug "lib/completion", from:oh-my-zsh
