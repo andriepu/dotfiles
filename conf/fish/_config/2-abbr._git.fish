4724983f990ed5faa5c90ccdad91dd2d5a447d6e
andriepu <andriepu@gmail.com>
2022-03-17 17:36:17 +0700

    Simplify flags

--- conf/fish/_config/2-abbr._git.fish
+++ conf/fish/_config/2-abbr._git.fish
@@ -1,14 +1,14 @@
-abbr -a -g gc 'git checkout'
-abbr -a -g gcm 'git checkout master'
-abbr -a -g gst 'git status -s'
-abbr -a -g gd 'git diff'
-abbr -a -g ga 'git add -A'
-abbr -a -g gbd 'git branch -D'
-abbr -a -g gcmsg 'git commit -m'
-abbr -a -g gacmsg 'git add -A; git commit -m'
-abbr -a -g gre 'git reset'
-abbr -a -g gresoft 'git reset --soft HEAD\^'
-abbr -a -g grehard 'git reset --hard HEAD\^'
-abbr -a -g glog 'git log --graph --pretty='\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --max-count=15'
-abbr -a -g gl "git pull; git pull origin (git branch | sed -n -e 's/^\* \(.*\)/\1/p')"
-abbr -a -g gp "git push origin (git branch | sed -n -e 's/^\* \(.*\)/\1/p')"
+abbr -ag gc 'git checkout'
+abbr -ag gcm 'git checkout master'
+abbr -ag gst 'git status -s'
+abbr -ag gd 'git diff'
+abbr -ag ga 'git add -A'
+abbr -ag gbd 'git branch -D'
+abbr -ag gcmsg 'git commit -m'
+abbr -ag gacmsg 'git add -A; git commit -m'
+abbr -ag gre 'git reset'
+abbr -ag gresoft 'git reset --soft HEAD\^'
+abbr -ag grehard 'git reset --hard HEAD\^'
+abbr -ag glog 'git log --graph --pretty='\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --max-count=15'
+abbr -ag gl "git pull; git pull origin (git branch | sed -n -e 's/^\* \(.*\)/\1/p')"
+abbr -ag gp "git push origin (git branch | sed -n -e 's/^\* \(.*\)/\1/p')"
