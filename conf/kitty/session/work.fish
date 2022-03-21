2b44cd1560d9e2c8abdae2608877f7fcf462efc4
andriepu <andriepu@gmail.com>
2022-03-21 09:36:08 +0700

    Update kitty session

--- conf/kitty/session/work.fish
+++ conf/kitty/session/work.fish
@@ -3,20 +3,25 @@
 kitty @ set-window-title Initial-Service
 
 kitty @ launch --no-response --title Mock-User-Context --cwd "$HOME/Workspace/mock-user-context/" fish
-kitty @ launch --no-response --title Majapahit --cwd "$HOME/Workspace/majapahit" fish -C 'nvm use 14; yarn dev'
-kitty @ launch --no-response --title Essos --cwd "$HOME/Workspace/essos" fish -C 'nvm use 14; yarn dev'
-kitty @ launch --no-response --title Westeros --cwd "$HOME/Workspace/westeros/" fish -C 'nvm use 14; yarn dev'
+kitty @ launch --no-response --title Majapahit --cwd "$HOME/Workspace/majapahit" fish
+kitty @ launch --no-response --title Essos --cwd "$HOME/Workspace/essos" fish
+kitty @ launch --no-response --title Westeros --cwd "$HOME/Workspace/westeros/" fish
 kitty @ launch --no-response --title Hahen --cwd "$HOME/Workspace/hahen" fish -C "nvm use 14; yarn dev"
-kitty @ launch --no-response --title Quest --cwd "$HOME/Workspace/quest/" fish -C "nvm use 14; yarn start"
-kitty @ launch --no-response --title Sievex-Frontend --cwd "$HOME/Workspace/sievex-frontend/" fish -C "nvm use 14; yarn start"
+kitty @ launch --no-response --title Quest --cwd "$HOME/Workspace/quest/" fish
+kitty @ launch --no-response --title Sievex-Frontend --cwd "$HOME/Workspace/sievex-frontend/" fish
 
-kitty @ send-text --match title:Mock-User-Context 'yarn start '
+kitty @ send-text --match title:Mock-User-Context "yarn start "
+kitty @ send-text --match title:Majapahit "nvm use 14; yarn dev"
+kitty @ send-text --match title:Essos "nvm use 14; yarn dev"
+kitty @ send-text --match title:Westeros "nvm use 14; yarn dev"
+kitty @ send-text --match title:Quest "nvm use 14; yarn start"
+kitty @ send-text --match title:Sievex-Frontend "nvm use 14; yarn start"
 
 kitty @ launch --no-response --title VPN-Preprod-Azure --type tab --cwd "$HOME/Workspace/.vpn" fish
 kitty @ launch --no-response --title VPN-Prod-Gcp --cwd "$HOME/Workspace/.vpn" fish
 
-kitty @ send-text --match title:VPN-Preprod-Azure 'ds-blvpn bl-azure-cloud-preproduction-00-udp.ovpn'
-kitty @ send-text --match title:VPN-Prod-Gcp 'ds-blvpn vm-openvpn-production-02.ovpn'
+kitty @ send-text --match title:VPN-Preprod-Azure "ds-blvpn azure-preproduction"
+kitty @ send-text --match title:VPN-Prod-Gcp "ds-blvpn gcp-production"
 
 kitty @ launch --no-response --title Neovim --type tab --cwd "$HOME/Workspace/" fish -C "nvim"
 
